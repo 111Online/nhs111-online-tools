c63e0b17d0ba59b8db61e30fbb07442f55064219
Shane Hudson <shane@shanehudson.net>
2019-10-02 15:04:25 +0100

    Adding the data that is no longer shown in columns into a modal info panel

--- NHS111.Online.Tools.Web/wwwroot/js/site.js
+++ NHS111.Online.Tools.Web/wwwroot/js/site.js
@@ -1,4 +1,14 @@
-﻿// Please see documentation at https://docs.microsoft.com/aspnet/core/client-side/bundling-and-minification
-// for details on configuring this project to bundle and minify static web assets.
+﻿$('#infoModal').on('show.bs.modal', function (event) {
+    var button = $(event.relatedTarget) // Button that triggered the modal
+    var data = button.data('json') // Extract info from data-* attributes
 
-// Write your JavaScript code.
+    var modal = $(this)
+    var str = ""
+    
+    for (var i = 0; i < Object.keys(data).length; i++) {
+        var key = Object.keys(data)[i]
+        str += "<p>" + key + ": " + data[key] + "</p>"
+    }
+
+    modal.find('.modal-body').html(str)
+})
